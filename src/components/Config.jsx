451d1c11b7199ac7803a6f5c6cd8512b67e4d5b1
AifaDev <iabodfah@gmail.com>
2023-01-20 19:46:23 +0300

    http

--- src/components/Config.jsx
+++ src/components/Config.jsx
@@ -58,7 +58,7 @@ export default function Config() {
           <svg
             onClick={() => toggleConfig(false)}
             className="absolute right-6 top-5 cursor-pointer"
-            xmlns="http://www.w3.org/2000/svg"
+            xmlns="https://www.w3.org/2000/svg"
             width="41"
             height="41"
             fill="none"
