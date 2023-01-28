a9ca2e919e16d66c7cbacec70e3d10b090f1716a
AifaDev <iabodfah@gmail.com>
2023-01-28 11:54:51 +0300

    Linkedin + icon

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,14 @@ function App() {
   const { openedConfig, theme, keyboard, progress } = useKeys();
 
   useLayoutEffect(() => {
-    if((Date.now() - localStorage.getItem("progressedAt")) > 86400000 || !localStorage.getItem("progressedAt")){
-      localStorage.setItem("progressedAt", Date.now())
-      localStorage.setItem("progress", 0)
+    if (
+      Date.now() - localStorage.getItem("progressedAt") > 86400000 ||
+      !localStorage.getItem("progressedAt")
+    ) {
+      localStorage.setItem("progressedAt", Date.now());
+      localStorage.setItem("progress", 0);
     }
-  }, [progress])
+  }, [progress]);
   useLayoutEffect(() => {
     if (
       localStorage.theme === "Dark" ||
@@ -44,16 +47,34 @@ function App() {
           }}`}
         >
           <Brief className="h-28 flex flex-col justify-start" />
-          {<Paragraph
-            className={`sm:block sm:h-auto flex flex-col justify-center short:flex short:justify-center short:h-full h-full outline-none ${
-              keyboard === "Show" ? "" : "pt-[5%]"
-            }`}
-          />}
+          {
+            <Paragraph
+              className={`sm:block sm:h-auto flex flex-col justify-center short:flex short:justify-center short:h-full h-full outline-none ${
+                keyboard === "Show" ? "" : "pt-[5%]"
+              }`}
+            />
+          }
           <Keyboard
             className={`md:w-[920px] sm:w-[800px] sm:flex short:hidden hidden aspect-[2.86559541604] bg-keyboard dark:bg-keyboard-dark self-center justify-center items-center p-2 rounded-md ${
               keyboard === "Show" ? "" : "!hidden"
             }`}
           />
+        </div>
+        <div className="absolute bottom-0 right-0 p-4">
+          <a
+            href="https://www.linkedin.com/in/abdullah-hakeem/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <svg
+              className="dark:fill-[#14181d] fill-[#cccccc] hover:fill-bar-light dark:hover:fill-bar-dark transition-colors duration-200"
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+            >
+              <path d="M41 4H9C6.24 4 4 6.24 4 9v32c0 2.76 2.24 5 5 5h32c2.76 0 5-2.24 5-5V9c0-2.76-2.24-5-5-5zM17 20v19h-6V20h6zm-6-5.53c0-1.4 1.2-2.47 3-2.47s2.93 1.07 3 2.47c0 1.4-1.12 2.53-3 2.53-1.8 0-3-1.13-3-2.53zM39 39h-6V29c0-2-1-4-3.5-4.04h-.08C27 24.96 26 27.02 26 29v10h-6V20h6v2.56S27.93 20 31.81 20c3.97 0 7.19 2.73 7.19 8.26V39z" />
+            </svg>{" "}
+          </a>
         </div>
       </div>
     </>
